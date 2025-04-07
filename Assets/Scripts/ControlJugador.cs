332f562d2222471d02537b9d7f3da382e37a96f7
sereto111 <sereto562@gmail.com>
2025-04-07 09:55:58 +0200

    Actualizar controles del jugador: ajustar tipo de control a Vector2, implementar movimiento y rotación del jugador, y modificar parámetros de cámara.

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -18,6 +18,11 @@ public class ControlJugador : MonoBehaviour
         ratonDelta = context.ReadValue<Vector2>();
     }
 
+    private void FixedUpdate()
+    {
+        MovimientoJugador();
+    }
+
     private void LateUpdate()
     {
         VistaCamara();
@@ -27,6 +32,37 @@ public class ControlJugador : MonoBehaviour
     {
         rotacionActualCamara += ratonDelta.y * sensibilidadRaton;
         rotacionActualCamara = Mathf.Clamp(rotacionActualCamara, minVistaX, maxVistaX);
-        camara.localEulerAngles = new Vector3(-rotacionActualCamara, 0, 0);
+        camara.localEulerAngles = new Vector3(-rotacionActualCamara, 0, 0); // Rotacion de la camara (arriba y abajo)
+        transform.eulerAngles += new Vector3(0, ratonDelta.x * sensibilidadRaton, 0); // Rotacion del jugador
+    }
+
+    [Header("Movimiento Jugador")]
+    public float velocidadMovimiento;
+    private Vector2 movimientoActualEntrada;
+    private Rigidbody fisica;
+
+    private void Awake()
+    {
+        fisica = GetComponent<Rigidbody>();
+    }
+
+    public void OnMovimientoInput(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            movimientoActualEntrada = context.ReadValue<Vector2>();
+        }
+        else if(context.phase == InputActionPhase.Canceled)
+        {
+            movimientoActualEntrada = Vector2.zero;
+        }
+    }
+
+    private void MovimientoJugador()
+    {
+        Vector3 direccion = transform.forward * movimientoActualEntrada.y + transform.right * movimientoActualEntrada.x; // Movimiento en la direccion del jugador
+        direccion *= velocidadMovimiento; // Velocidad del jugador
+        direccion.y = fisica.velocity.y; // Mantener la velocidad vertical (gravedad)
+        fisica.velocity = direccion; // Aplicar la velocidad al jugador
     }
 }
