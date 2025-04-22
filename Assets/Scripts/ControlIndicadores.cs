ed8d999e98716147e904885e6990773d798c2c6d
sereto111 <sereto562@gmail.com>
2025-04-22 10:13:06 +0200

    Agregar control de deterioro y salud del jugador, incluyendo nuevas funcionalidades para manejar indicadores y eventos de muerte.

--- Assets/Scripts/ControlIndicadores.cs
+++ Assets/Scripts/ControlIndicadores.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
-public class ControlIndicadores : MonoBehaviour
+public class ControlIndicadores : MonoBehaviour, IDeterioro
 {
     public Indicador hambre;
     public Indicador sed;
     public Indicador descanso;
     public Indicador salud;
+    public TextMeshProUGUI muerteTxt;
+
+    public UnityEvent OnSufrirDeterioro;
 
     // variables para reducir la salud cuando se acaba la comida o la bebida
     public float reduccionSaludConHambre;
@@ -27,28 +32,68 @@ public class ControlIndicadores : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Actualizar la barra de hambre
-        hambre.barraUI.fillAmount = hambre.ObtenerPorcentaje(hambre.valorActual, hambre.valorMaximo);
-        hambre.Restar(hambre.indiceDeterioro * Time.deltaTime);
+        if(hambre.valorActual <= 0.0f || sed.valorActual <= 0.0f){
+            // Reducir salud si se acaba la comida o bebida
+            if (hambre.valorActual <= 0.0f)
+                salud.Restar(reduccionSaludConHambre * Time.deltaTime);
 
-        // Actualizar la barra de sed
-        sed.barraUI.fillAmount = sed.ObtenerPorcentaje(sed.valorActual, sed.valorMaximo);
-        sed.Restar(sed.indiceDeterioro * Time.deltaTime);
+            if (sed.valorActual <= 0.0f)
+                salud.Restar(reduccionSaludConSed * Time.deltaTime);
+        } else {
+            // Actualizar la barra de hambre
+            hambre.barraUI.fillAmount = hambre.ObtenerPorcentaje(hambre.valorActual, hambre.valorMaximo);
+            hambre.Restar(hambre.indiceDeterioro * Time.deltaTime);
 
-        // Actualizar la barra de descanso
-        descanso.barraUI.fillAmount = descanso.ObtenerPorcentaje(descanso.valorActual, descanso.valorMaximo);
-        descanso.Sumar(descanso.indiceRecuperacion * Time.deltaTime);
+            // Actualizar la barra de sed
+            sed.barraUI.fillAmount = sed.ObtenerPorcentaje(sed.valorActual, sed.valorMaximo);
+            sed.Restar(sed.indiceDeterioro * Time.deltaTime);
 
-        // Actualizar la barra de salud
-        salud.barraUI.fillAmount = salud.ObtenerPorcentaje(salud.valorActual, salud.valorMaximo);
-        //salud.Restar(salud.indiceDeterioro * Time.deltaTime);
+            // Actualizar la barra de descanso
+            descanso.barraUI.fillAmount = descanso.ObtenerPorcentaje(descanso.valorActual, descanso.valorMaximo);
+            descanso.Sumar(descanso.indiceRecuperacion * Time.deltaTime);
 
-        // Reducir salud si se acaba la comida o bebida
-        /*if (hambre.valorActual <= 0.0f)
-            salud.Restar(reduccionSaludConHambre * Time.deltaTime);
+            // Actualizar la barra de salud
+            salud.barraUI.fillAmount = salud.ObtenerPorcentaje(salud.valorActual, salud.valorMaximo);
+            //salud.Restar(salud.indiceDeterioro * Time.deltaTime);
+        }
 
-        if (sed.valorActual <= 0.0f)
-            salud.Restar(reduccionSaludConSed * Time.deltaTime);*/
+        // Comprobar si el jugador ha muerto
+        if (salud.valorActual <= 0.0f)
+            Muerto();                  
+    }
+
+    public void Muerto()
+    {        
+        Time.timeScale = 0.0f;
+        muerteTxt.gameObject.SetActive(true);
+    }
+
+    public void IncrementarIndicadores(string indicador, float cantidad)
+    {
+        switch (indicador.ToLower())
+        {
+            case "hambre":
+                hambre.Sumar(cantidad);
+                break;
+            case "sed":
+                sed.Sumar(cantidad);
+                break;
+            case "descanso":
+                descanso.Sumar(cantidad);
+                break;
+            case "salud":
+                salud.Sumar(cantidad);
+                break;
+            default:
+                Debug.Log("ERROR: Indicador no reconocido - " + indicador);
+                break;
+        }
+    }
+
+    public void ProducirDeterioro(float cantidad)
+    {
+        salud.Restar(cantidad);
+        OnSufrirDeterioro?.Invoke();
     }
 }
 
@@ -91,4 +136,9 @@ public class Indicador
     {
         return valorActual / valorMaximo;
     }
+}
+
+public interface IDeterioro
+{
+    void ProducirDeterioro(float cantidadDeterioro);
 }
---
+++ Assets/Scripts/SetaVenenosa.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetaVenenosa : MonoBehaviour
+{
+    public float cantidadVeneno;
+    public float indiceDeterioro;
+
+    private List<IDeterioro> listaParaDeteriorar = new List<IDeterioro>(); // Lista de objetos que se deterioran al entrar en contacto con la seta venenosa
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OllisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+            Debug.Log("El jugador ha tocado una seta venenosa.");
+        }       
+    }
+
+    private void OnCsionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("El jugador ha dejado de tocar una seta venenosa.");
+        }             
+    }
+}
