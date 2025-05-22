fee351c8aabd11dbeefd7638415ca89995281056
sereto111 <sereto562@gmail.com>
2025-05-22 11:30:05 +0200

    Refactor code structure for improved readability and maintainability

--- Assets/Scripts/ControlInventario.cs
+++ Assets/Scripts/ControlInventario.cs
@@ -1,18 +1,144 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ControlInventario : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ElementosEstanteriaUI[] elementosEstanteriaUIs; // Array de elementos del inventario
+    public ElementosEstanteria[] elementosEstanteria; // Array de elementos de la estantería
+    public GameObject ventanaInventario; // Ventana del inventario
+
+    public Transform posicionSoltar; // Posición de la estantería
+    [Header("elementos seleccionados")]
+    private ElementosEstanteria elementosEstanteriaSeleccionado; // Elemento seleccionado
+    private int IndiceElementoSeleccionado; // Índice del elemento seleccionado
+    public TextMeshProUGUI nombreElementoSeleccionado; // Nombre del elemento seleccionado
+    public TextMeshProUGUI descripcionElementoSeleccionado; // Descripcion del elemento seleccionado
+    public TextMeshProUGUI nombreNecesidadElementoSeleccionado; // nombreNecesidad del elemento seleccionado
+    public TextMeshProUGUI valoresNecesidadElementoSeleccionado; // valoresNecesidad del elemento seleccionado
+    public Button botonUsarElementoSeleccionado; // Botón de usar el elemento seleccionado
+    public Button botonSoltarElementoSeleccionado; // Botón de soltar el elemento seleccionado
+
+    //necesitamos el control del jugador para poder usar el elemento
+    private ControlJugador controlJugador; // Control del jugador
+    [Header("eventos")]
+    public UnityEvent onabrirVentanaInventario; // Evento al abrir la ventana del inventario
+    public UnityEvent oncerrarVentanaInventario; // Evento al cerrar la ventana del inventario
+
+    //sigleton para mantener la informacion entre clases
+    public static ControlInventario instance; // Instancia del singleton
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        //aprovecho el awake para inicializar el control del jugador
+        controlJugador = GetComponent<ControlJugador>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        ventanaInventario.SetActive(false); // Desactivar la ventana del inventario
+        elementosEstanteria = new ElementosEstanteria[elementosEstanteriaUIs.Length];
+        for (int i = 0; i < elementosEstanteriaUIs.Length; i++)
+        {
+            elementosEstanteria[i] = new ElementosEstanteria(); // Inicializar el array de elementos de la estantería
+            elementosEstanteriaUIs[i].indice = i; // Asignar el índice del elemento
+            elementosEstanteriaUIs[i].Limpiar(); // Limpiar el elemento
+        }
     }
+
+    void AbrirCerrarVentanaInventario()
+    {
+
+    }
+
+    // Consulta si la ventana del inventario esta abierta o cerrada
+    bool EstaAbierto()
+    {
+        return ventanaInventario.activeInHierarchy; // Comprobar si la ventana del inventario está abierta
+    }
+
+    // TOFIX
+    // TODO
+    // Método para actualizar el inventario
+    /*void AnadirElemento(DatosElemento datosElemento) 
+    {
+        ElementosEstanteria elementoParaAlmacenar = ObtenerElementoAlmacenado(datosElemento); // Obtener el elemento almacenado en la estantería
+        if (elementoParaAlmacenar != null)
+        {
+            elementoParaAlmacenar.cantidad++; // Aumentar la cantidad del elemento almacenado
+            ActualizarUI(); // Actualizar la interfaz de usuario
+            return;
+        }
+
+        ElementosEstanteria objetoVacio = ObtenerObjetoVacio(); // Obtener un objeto vacío
+        if (objetoVacio != null)
+        {
+            objetoVacio.datosElemento = datosElemento; // Asignar el elemento a la posición vacía
+            objetoVacio.cantidad = 1; // Inicializar la cantidad del elemento
+            ActualizarUI(); // Actualizar la interfaz de usuario
+            return;
+        }
+        else
+        {
+            Debug.Log("No hay espacio en el inventario"); // Mensaje de error si no hay espacio en el inventario
+        }
+    }*/
+
+    void SoltarElemento(DatosElemento elemento)
+    {
+
+    }
+
+    void ActualizarUI()
+    {
+
+    }
+
+    ElementosEstanteria ObtenerElementoAlmacenado(int indice)
+    {
+        return null; //TODO
+    }
+
+    ElementosEstanteria ObtenerObjetoVacio()
+    {
+        return null; //TODO
+    }
+
+    void ElementoSeleccionado(int indice)
+    {
+        //TODO
+    }
+
+    void EliminarElementoSeleccionado(int indice)
+    {
+        //TODO
+    }
+
+    public void OnBotonUsar()
+    {
+        //TODO
+    }
+
+    public void OnBotonSoltar()
+    {
+        //TODO
+    }
+}
+
+public class ElementosEstanteria
+{
+    public DatosElemento datosElemento; // Datos del elemento
+    public int cantidad; // Cantidad del elemento
 }
---
+++ Assets/Scripts/ElementosEstanteriaUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElementosEstanteriaUI : MonoBehaviour
+{
+    public Button botonElemento; // Botón del elemento
+    public Image icono; // Imagen del elemento
+    public TextMeshProUGUI textoCantidad; // Nombre del elemento
+    public int indice; // Índice del elemento
+    private ElementosEstanteria elementoactual; // Elemento actual
+
+    public void Establecer(ElementosEstanteria elemento)
+    {
+        elementoactual = elemento;
+        icono.gameObject.SetActive(true); // Activar la imagen del elemento
+        icono.sprite = elemento.datosElemento.icono; // Asignar el icono del elemento
+        //comprueba que el elemento sea mayor que 1 y si no lo pone vacio
+        textoCantidad.text = elemento.cantidad > 1 ? elemento.cantidad.ToString() : string.Empty; // Asignar la cantidad del elemento
+    }
+    public void Limpiar()
+    {
+        elementoactual = null; // Limpiar el elemento actual
+        icono.gameObject.SetActive(false); // Desactivar la imagen del elemento
+        textoCantidad.text = ""; // Limpiar el texto de la cantidad
+    }
+
+
+    
+}
